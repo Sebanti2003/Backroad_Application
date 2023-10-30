02068ab79fb52e59fbdb22ee443b8c62f08cb323
Sebanti2003 <dasguptasebanti2003@gmail.com>
2023-10-30 18:23:37 +0530

    commited

--- src/assets/components/Pertour.jsx
+++ src/assets/components/Pertour.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Pertour(props) {
   return (
     <>
-     <div className='w-[270px] h-[340px] bg-slate-200 overflow-hidden rounded-md m-3 relative border-[1px] border-black'>
+     <div className='w-[270px] h-[340px] bg-slate-200 overflow-hidden rounded-md m-3 relative border-[1px] border-black' key={props.key}>
         <div className='h-[50%] w-full border-b-2 border-black relative'>
             <img src={props.img} className='w-full h-full object-cover absolute z-[3] top-0 right-0 left-0 bottom-0' alt="" />
             <div className='absolute z-[4] bg-orange-500 rounded-lg text-white bottom-0 right-0'>{props.date}</div>
--- src/assets/components/Service.jsx
+++ src/assets/components/Service.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function Service(props) {
-    const {img,title,desc}=props;
+    const {key,img,title,desc}=props;
   return (
     <>
     
-     <div className='flex  mx-2 w-[310px] h-[100px] justify-center items-center gap-1 max-lg:mt-5 '>
+     <div className='flex  mx-2 w-[310px] h-[100px] justify-center items-center gap-1 max-lg:mt-5 ' key={key}>
         <img src={img} alt="" className='w-[20%] h-full'/>
         <div className='flex flex-col w-[80%] h-full '>
             <h1 className='tracking-widest font-mono font-bold'>{title}</h1>
