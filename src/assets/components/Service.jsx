ae3a1048fe3bedd031464021eee9d887b612793c
Sebanti2003 <dasguptasebanti2003@gmail.com>
2023-10-30 18:35:26 +0530

    commited

--- src/assets/components/Service.jsx
+++ src/assets/components/Service.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
 function Service(props) {
-    const {key,img,title,desc}=props;
   return (
     <>
     
-     <div className='flex  mx-2 w-[310px] h-[100px] justify-center items-center gap-1 max-lg:mt-5 ' key={key}>
-        <img src={img} alt="" className='w-[20%] h-full'/>
+     <div className='flex  mx-2 w-[310px] h-[100px] justify-center items-center gap-1 max-lg:mt-5 '>
+        <img src={props.img} alt="" className='w-[20%] h-full'/>
         <div className='flex flex-col w-[80%] h-full '>
-            <h1 className='tracking-widest font-mono font-bold'>{title}</h1>
-            <p className='text-slate-500'>{desc}</p>
+            <h1 className='tracking-widest font-mono font-bold'>{props.title}</h1>
+            <p className='text-slate-500'>{props.desc}</p>
         </div>
     </div>
     </>
