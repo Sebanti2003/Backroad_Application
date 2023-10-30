b2d41362137863ed7faf401c81c74ae2795758c1
Sebanti2003 <dasguptasebanti2003@gmail.com>
2023-10-30 08:40:17 +0530

    commited

--- src/assets/components/Service.jsx
+++ src/assets/components/Service.jsx
@@ -5,13 +5,12 @@ function Service(props) {
   return (
     <>
     
-     <div className='flex w-[300px] h-[100px] justify-center items-center gap-1 max-md:mt-14 '>
+     <div className='flex  mx-2 w-[310px] h-[100px] justify-center items-center gap-1 max-lg:mt-14 '>
         <img src={img} alt="" className='w-[20%] h-full'/>
         <div className='flex flex-col w-[80%] h-full '>
             <h1 className='tracking-widest font-mono font-bold'>{title}</h1>
             <p className='text-slate-500'>{desc}</p>
         </div>
-      
     </div>
     </>
    
